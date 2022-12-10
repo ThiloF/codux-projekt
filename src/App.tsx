8edb508c2c7d5b59bdd6ed06f9559e73e100ecb2
ThiloF <thilo-falkenstein@unitybox.de>
2022-12-10 20:43:13 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    Learn React
+                    Moin Moin
                 </a>
             </header>
         </div>
